f19e8f322bd1e685ebdcbeac5d806e3e2f77747c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-06-01 16:29:40 +0300

    Update Annihilation.php

--- src/mfdgaming/annihilation/Annihilation.php
+++ src/mfdgaming/annihilation/Annihilation.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace mfdgaming\annihilation;
+
+use pocketmine\plugin\PluginBase;
+
+class Annihilation extends PluginBase {
+	public function onLoad() {
+		//do something
+	}
+
+	public function onEnable() {
+		//do something
+	}
+
+	public function onDisable() {
+		//do something
+	}
+}
