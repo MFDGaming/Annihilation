32fc924c424e715a0aa108d8822f2cc6bf98dd5c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-06-01 17:04:37 +0300

    Update EventListener.php

--- src/mfdgaming/annihilation/event/EventListener.php
+++ src/mfdgaming/annihilation/event/EventListener.php
@@ -5,5 +5,10 @@ namespace mfdgaming\annihilation\event;
 use pocketmine\event\Listener;
 
 class EventListener extends Listener {
+	public function __construct(Main $main) {
+		parent::__construct($main);
+		$this->plugin = $main;
+	}
+  
 
 }
